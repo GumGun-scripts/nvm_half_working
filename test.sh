3d7b6e5351eb2e7e661512e262bea6b127e6f9ed
gummygun <gonzalo.godet@gmail.com>
2023-04-28 13:17:36 -0600

    iter2.0

--- test.sh
+++ test.sh
@@ -3,8 +3,21 @@
 curl https://raw.githubusercontent.com/GumGun-scripts/vimConfig/main/setupVim.sh | sh
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
-. .bashrc
+. ~/.bashrc
 nvm install --lts
+. ~/.bashrc
 npm install pm2@latest -g
-
+. ~/.bashrc
+sudo apt-get install authbind
+sudo touch /etc/authbind/byport/80
+sudo chown $SUDO_USER /etc/authbind/byport/80
+sudo chmod 755 /etc/authbind/byport/80
+git clone https://github.com/aparres/ITESO_CC_SlowHelloWorldNode code
+cd code
+authbind --deep pm2 update
+authbind --deep pm2 start web.js
+pm2 startup systemd
+sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+pm2 save
+sudo systemctl start pm2-ubuntu.service
 
