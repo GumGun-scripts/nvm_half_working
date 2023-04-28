bd6265f68a46ba3bef6913d403511d8076cfff9b
gummygun <gonzalo.godet@gmail.com>
2023-04-28 13:32:51 -0600

    try 3, no longer a script

--- test.sh
+++ test.sh
@@ -1,5 +1,3 @@
-#!/bin/bashasdfasd
-
 curl https://raw.githubusercontent.com/GumGun-scripts/vimConfig/main/setupVim.sh | sh
 curl -sL https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh -o install_nvm.sh
 bash install_nvm.sh
@@ -17,7 +15,7 @@ cd code
 authbind --deep pm2 update
 authbind --deep pm2 start web.js
 pm2 startup systemd
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
+sudo env PATH=$PATH:/home/ubuntu/.nvm/versions/node/v18.16.0/bin /home/ubuntu/.nvm/versions/node/v18.16.0/lib/node_modules/pm2/bin/pm2 startup systemd -u ubuntu --hp /home/ubuntu
 pm2 save
 sudo systemctl start pm2-ubuntu.service
 
